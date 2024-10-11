13794df986153ed4364f6dad95befd53057e0ed4
mirbek10 <m51869956@gmail.com>
2024-10-11 18:19:36 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,86 @@
+// if else , switch case 
+// [] {}
+
+// цикл
+// while , do while , for 
+
+// let num = 0 
+
+// while (num <10) { console.log(num);
+    
+
+
+// let num = 0 
+// do {
+//     console.log(num);
+//     num++;
+// } while (num<=30);
+
+//     for (let i = 20; i >= 0; i--) {
+//      console.log(i);
+      
+     
+//     }
+// for (let i = 0; i <=10; i ++){
+//      if (i === 0)
+//            {
+//           console.log( i );
+//      }
+    
+
+// if (hgvgghvgv) {
+     
+// } else {
+     let num 
+
+ do {
+     num =prompt('сан жаз')
+ } while (num<=10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
